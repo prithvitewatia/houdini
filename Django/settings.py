2afa8a7fe9827968b89680d3cb3296db5c1565b4
Prithvi <prithvisinghtewatia@gmail.com>
2022-07-27 15:51:01 +0530

    secured secret key

--- Django/settings.py
+++ Django/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6xp+_27yn0gwnsyn!_+fn))hl*b#7zhfui*tztdxu9-1xu_nl-'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
